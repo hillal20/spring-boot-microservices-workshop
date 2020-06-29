e406b69331c1ef35fe17cf76fa91506d73e8177e
Hilal Aissani <hilalaissani@gmail.com>
2020-06-29 12:26:40 -0500

    latest

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -4,6 +4,7 @@ import io.javabrains.movieinfoservice.models.Movie;
 import io.javabrains.movieinfoservice.models.MovieSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RefreshScope
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
